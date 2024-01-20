df266e1f38987638ec313af7408f8f7b5f611ac8
PistonShot1 <nandaprian7@gmail.com>
2024-01-20 22:19:40 +0800

    render start screen

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+-- Adding android permission 
+Add permissions to your AndroidManifest.xml file. There are debug, main, and profile versions which are chosen depending on how you start your app. In general, it's sufficient to add permission only to the main version. Here's an example AndroidManifest.xml with a complete list of all possible permissions.
---
+++ lib/tiledcomponenet.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/material.dart';
+
+class MyGame extends FlameGame {
+  late Sprite backgroundImage;
+
+  @override
+  Future<void> onLoad() async {
+    await Flame.images.load('main-menu-background.jpg');
+    backgroundImage =
+        Sprite(Flame.images.fromCache('main-menu-background.jpg'));
+  }
+
+  @override
+  void render(Canvas canvas) {
+    // Clear the canvas
+    canvas.drawRect(
+        Rect.fromLTWH(0, 0, size.x, size.y), Paint()..color = Colors.white);
+
+    // Render the background image
+    backgroundImage.renderRect(canvas, Rect.fromLTWH(0, 0, size.x, size.y));
+  }
+}
