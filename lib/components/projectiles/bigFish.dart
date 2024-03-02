c4322ad0535236a7462225d38ebe3f7e91d76787
PistonShot1 <nandaprian7@gmail.com>
2024-03-02 20:14:38 +0800

    fix on mine spawning position

--- lib/components/projectiles/bigFish.dart
+++ lib/components/projectiles/bigFish.dart
@@ -11,11 +11,12 @@ import 'package:oceanoasis/components/Boss/overworldplayer.dart';
 import 'package:oceanoasis/components/projectiles/mine.dart';
 import 'package:oceanoasis/routes/homescreen.dart';
 
-class bigFish extends SpriteAnimationComponent with HasGameReference<MyGame>, CollisionCallbacks{
+class bigFish extends SpriteAnimationComponent
+    with HasGameReference<MyGame>, CollisionCallbacks {
   final int destroyTime = 5;
   double projectileSpeed = 666;
   late double projectileDirection;
-  final Vector2 initialSize = Vector2(300,300);
+  final Vector2 initialSize = Vector2(300, 300);
   final double hitboxOffsetX = 100;
   final double hitboxOffsetY = 70;
   final double spawnOffsetX = -500;
@@ -30,47 +31,45 @@ class bigFish extends SpriteAnimationComponent with HasGameReference<MyGame>, Co
     currentWorld = w;
     super.position = sourcePosition + Vector2(spawnOffsetX, spawnOffsetY);
     super.size = initialSize;
-
   }
-  
+
   @override
-  FutureOr<void> onLoad() async{
+  FutureOr<void> onLoad() async {
     Image image = await Flame.images.load('bossfight/fish-big.png');
     priority = 3;
     animation = SpriteAnimation.fromFrameData(
         image,
         SpriteAnimationData.sequenced(
             amount: 4, stepTime: 0.1, textureSize: Vector2(54, 54)));
-       CircleHitbox c = CircleHitbox.relative(0.6, parentSize: initialSize, position: Vector2(hitboxOffsetX, hitboxOffsetY));
+    CircleHitbox c = CircleHitbox.relative(0.6,
+        parentSize: initialSize,
+        position: Vector2(hitboxOffsetX, hitboxOffsetY));
 
-      add(c);
-      debugMode = true;
+    add(c);
+    debugMode = true;
     return super.onLoad();
   }
 
   void removeComponent() {
-      Future.delayed(const Duration(seconds: 15), () {
-        super.removeFromParent();
-      });
+    Future.delayed(const Duration(seconds: 15), () {
+      super.removeFromParent();
+    });
   }
 
   @override
   void onCollisionStart(
       Set<Vector2> intersectionPoints, PositionComponent other) {
     super.onCollisionStart(intersectionPoints, other);
-    if (other is OverworldPlayer){
+    if (other is OverworldPlayer) {
       other.takeDamage(damageToPlayer);
     }
-    if (other is freezeEffect && frozen == false){
+    if (other is freezeEffect && frozen == false) {
       freezeEvent();
     }
-
   }
 
   @override
   void onCollisionEnd(PositionComponent other) {
-
-
     super.onCollisionEnd(other);
   }
 
@@ -78,34 +77,47 @@ class bigFish extends SpriteAnimationComponent with HasGameReference<MyGame>, Co
   final double mineSpawnOffsetY = 50;
 
   void spawnMine() {
-    if (mineCooldown == false && frozen == false ){
+    if (mineCooldown == false && frozen == false) {
       mineCooldown = true;
+      bool check = true;
       Future.delayed(const Duration(seconds: 1), () {
-        mine m = mine(super.position, currentWorld, Vector2(mineSpawnOffsetX, mineSpawnOffsetY));
-        currentWorld.add(m);
+        mine m = mine(super.position, currentWorld,
+            Vector2(mineSpawnOffsetX, mineSpawnOffsetY));
+
+        currentWorld.children.whereType<mine>().forEach((mine element) {
+          if (m.position.x >= (element.position.x-100) &&
+              m.position.x <= (element.position.x + element.size.x+100) &&
+              m.position.y >= element.position.y - 50 &&
+              m.position.y <= (element.position.y + 50)) {
+            check = false;
+          }
+        });
+        if (check) {
+          currentWorld.add(m);
+          print('M new : ${m.position}');
+        }
+
         mineCooldown = false;
       });
     }
-
   }
+
   bool frozen = false;
 
-  void freezeEvent(){
+  void freezeEvent() {
     frozen = true;
     projectileSpeed = 0;
     Future.delayed(const Duration(seconds: 7), () {
-        projectileSpeed = 666;
-        frozen = false;
-      });
+      projectileSpeed = 666;
+      frozen = false;
+    });
   }
 
-
   @override
-  void update(double dt){
-    
-    position.x += (1* projectileSpeed * dt);
+  void update(double dt) {
+    position.x += (1 * projectileSpeed * dt);
 
-      spawnMine();
+    spawnMine();
 
     removeComponent();
     super.update(dt);
